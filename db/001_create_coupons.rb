3a3846b2b69c3e4e5f520d7a94227fdd0a979e02
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 06:44:52 +0000

    automatically backed up by learn

--- db/001_create_coupons.rb
+++ db/001_create_coupons.rb
@@ -2,9 +2,9 @@ class CreateCoupons < ActiveRecord::Migrate
 
   def change
     create_table :coupons do |t|
-      t.string :coupon_code 
-      t.string :store 
-    end 
-  end 
+      t.string :coupon_code
+      t.string :store
+    end
+  end
 
 end 
